fee0f87de74506f436d18aa65a79dc81754df71e
Vance Harris <53877381+harrisv25@users.noreply.github.com>
2024-04-30 13:33:08 -0600

    Add files via upload

--- Buffer.py
+++ Buffer.py
@@ -6,18 +6,12 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.linalg import VectorUDT
 from pyspark.sql.types import IntegerType
 from osgeo import gdal, ogr, osr
-import rasterio
-import rasterio.features
-import rasterio.warp
 import numpy as np
-# import geopandas as gpd
+import pandas as pd
+
 
 # https://spark.apache.org/docs/latest/ml-classification-regression.html
 
-# test = gpd.read_file('./data/input/roads/Study_Area_Roads.shp')
-
-# img = rasterio.open('data/input/image/LC09_L2SP_124051_20240407_20240408_02_T1_QA_PIXEL.TIF')
-# print(img.count, img.height, img.width, img.crs, img.dtypes, img.bounds)
 
 ref = gdal.Open('data/input/image/LC09_L2SP_124051_20240407_20240408_02_T1_SR_B1.TIF')
 
@@ -32,7 +26,7 @@ ymin = min(geo_t[3], geo_t[3] + y_size * geo_t[5])
 ymax = max(geo_t[3], geo_t[3] + y_size * geo_t[5])
 
 
-ds = gdal.Rasterize('./data/input/roads/Study_Area_Roads.tif', './data/input/roads/Study_Area_Roads_prj.shp', 
+ds = gdal.Rasterize('./data/input/roads/Study_Area_Roads.tif', './data/input/roads/Study_Area_Roads_prj/Study_Area_Roads_prj.shp', 
                     xRes=30, 
                     yRes=30,
                     format="GTIFF",
@@ -47,14 +41,16 @@ roads = np.array(ds.GetRasterBand(1).ReadAsArray())
 
 rds_idx = np.where(roads == 255)
 
-data = list(zip(rds_idx[0].astype(int), rds_idx[1]))
 
-# data = data[:10000]
+data = list(zip(rds_idx[0].astype(int), rds_idx[1].astype(int)))
+
+
+# data = data[:10]
 
 spark = SparkSession.builder.appName("Buffer").getOrCreate()
 df = spark.createDataFrame([([int(d[0]) - 1, int(d[0]), int(d[0]) + 1], 
-                             [int(d[1]) - 1, int(d[1]), int(d[1]) + 1]) for d in data], 
-                           ["OSM_RDS_IDX_X", "OSM_RDS_IDX_Y"])
+                                 [int(d[1]) - 1, int(d[1]), int(d[1]) + 1]) for d in data], 
+                               ["OSM_RDS_IDX_X", "OSM_RDS_IDX_Y"])
 
 
 df = df.select(explode(df.OSM_RDS_IDX_Y).alias("OSM_RDS_IDX_Y"), df.OSM_RDS_IDX_X)
@@ -67,8 +63,47 @@ df = df.dropDuplicates(["OSM_RDS_IDX_X", "OSM_RDS_IDX_Y"])
 df = df.withColumn("OSM_RDS_IDX_X", df["OSM_RDS_IDX_X"].cast(IntegerType()))
 df = df.withColumn("OSM_RDS_IDX_Y", df["OSM_RDS_IDX_Y"].cast(IntegerType()))
 
+print(1)
+
+# For Development purposes. Create a visual raster for training data selection
+# m1= np.array(df.select("OSM_RDS_IDX_X").collect()).ravel()
+# m2= np.array(df.select("OSM_RDS_IDX_Y").collect()).ravel()
+
+# sel_roads = np.zeros(roads.shape, dtype=int)
+
+# sel_roads[m1, m2] = 1
+
+# driver = gdal.GetDriverByName('GTIFF')
+# new = driver.Create('./data/input/roads/Sel_Roads.tif',
+    #                      ds.RasterXSize,
+    #                      ds.RasterYSize,
+    #                      1, 
+    #                      gdal.GDT_Int16
+    #                      )
+
+# new.GetRasterBand(1).WriteArray(sel_roads)
+# new.GetRasterBand(1).SetNoDataValue(-1)
+# new.SetGeoTransform(geo_t)
+# new.SetProjection(ds.GetProjection())
+
+
+dsr = gdal.Rasterize('./data/input/Training_Data.tif', './data/input/Training_Data/Training_Data.shp', 
+                    xRes=30, 
+                    yRes=30,
+                    format="GTIFF",
+                    allTouched=True, 
+                    attribute = "Road",
+                    outputBounds=[xmin, ymin, xmax, ymax], 
+                    noData = -1,
+                    outputType=gdal.GDT_Int16)
+
+
+# #road_values should be 255, else -1
+labels = np.array(dsr.GetRasterBand(1).ReadAsArray())
+
+
 def loc_img_value(x):
-    return int(array[int(x[0])][int(x[1])])
+        return int(array[int(x[0])][int(x[1])])
 
 for b in [i for i in os.listdir('data/input/image') if "B" in i]:
     b_label = b.split('_')[-1].split('.')[0]
@@ -91,16 +126,27 @@ for b in [i for i in os.listdir('data/input/image') if "B" in i]:
         df = df.withColumn("{}".format(b_label), 
                       ((col("{}".format(b_label)) * .00341802) + 149))
 
+print(2)
+
+def loc_lbl_value(x):
+        return int(labels[int(x[0])][int(x[1])])
+
+df = VectorAssembler(inputCols =["OSM_RDS_IDX_X", "OSM_RDS_IDX_Y"], 
+                          outputCol= "Label").transform(df)
+
+get_lbl_value = F.udf(lambda x: loc_lbl_value(x), IntegerType())
+
+df = df.withColumn("Label", get_lbl_value("Label"))
+
+df = df.where((df.B1 > 0) | 
+              (df.B2 > 0) | 
+              (df.B3 > 0) | 
+              (df.B4 > 0) |
+              (df.B5 > 0) |
+              (df.B6 > 0) |
+              (df.B7 > 0)
+              )
+
+print(3)
+
 df.describe().show()
-# vector =  VectorAssembler(inputCols =["OSM_RDS_IDX_X", "OSM_RDS_IDX_Y"], 
-#                           outputCol= "b1").transform(df)
-# def loc_img_value(x):
-#     return int(array[int(x[0])][int(x[1])])
-
-# get_img_value = F.udf(lambda x: loc_img_value(x), IntegerType())
-
-# vector = vector.withColumn("B1", get_img_value("b1"))
-
-# # vector.describe().show()
-
-
